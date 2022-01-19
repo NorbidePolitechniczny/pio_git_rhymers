afeaece01f988de7cac6bb82acd3965bc518392c
NorbidePolitechniczny <228722@edu.p.lodz.pl>
2022-01-19 14:30:45 +0100

    1.3.5 poprawa nazewnictwa metod

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -19,7 +19,7 @@ public class DefaultCountingOutRhymer {
         return total == 11;
     }
 
-    protected int peekaboo() {
+    protected int getNumbers() {
         if (callCheck())
             return -1;
         return numbers[total];
