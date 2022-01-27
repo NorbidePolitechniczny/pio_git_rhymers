b7455ed0020dc1e1940b3e6b21bd54888b7c5d82
NorbidePolitechniczny <228722@edu.p.lodz.pl>
2022-01-27 23:43:54 +0100

    1.5.6 Dodanie dokumentacji

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,14 @@
 package edu.kis.vh.nursery;
 
+/** DefaultCountingOutRhymer
+ * Klasa zajmująca się tablicą intów
+ * Posiada metody takie jak:
+ * Metoda dodająca element na koniec tablicy
+ * Metoda usuwająca element z końca tablicy
+ * Metoda sprawdzająca czy tablica jest pełna
+ * Metoda sprawdzająca czy tablica jest pusta
+ */
+
 public class DefaultCountingOutRhymer {
 
     public static final int INT = -1;
@@ -8,28 +17,47 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[INT1];
 
     private int total = INT;
+    /**
+     * getter zmiennej prywatnej total
+     * @return total
+     */
     public int getTotal() {
         return total;
     }
+    /**
+     * Metoda dodająca element na koniec tablicy
+     * @param in - element do dodania
+     */
     public void countIn(int in) {
         if (!isFull())
             this.numbers[++total] = in;
     }
-
+    /**
+     * Metoda sprawdzająca czy zmienna total jest równa -1 (const INT)
+     * @return true lub false
+     */
     public boolean callCheck() {
         return getTotal() == INT;
     }
-
+    /**
+     * Metoda sprawdzająca czy zmienna total jest równa 12 (const INT2)
+     * @return true lub false
+     */
     public boolean isFull() {
         return getTotal() == INT2;
     }
-
+    /**
+     * Metoda zwracająca ostatni element tablicy
+     * @return int[ostatni_element] lub -1 jeżeli jest pusta
+     */
     protected int getNumbers() {
         if (callCheck())
             return INT;
         return this.numbers[getTotal()];
     }
-
+    /**
+     * @return int[total -1] lub -1 jeżeli jest pusta
+     */
     public int countOut() {
         if (callCheck())
             return INT;
